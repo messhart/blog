eac77c3c6e2318c61bbb070e92bf4d9be0f66027
messhart <dyfttym@protonmail.ch>
2021-02-13 17:10:34 +0800

    change route

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,10 @@
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-rm -rf public
 
-hugo -t archie # Build a site to the ./public directory
+hugo # Build a site to the ./public directory
 cd public
-git add -A # Add all changes to git
+git add . # Add all changes to git
 
 # Commit changes
 msg="Rebuild site $(date)"
@@ -17,13 +16,12 @@ if [ -n "$*" ]; then
 fi
 git commit -m "$msg"
 
-
 # Push source and build repos
 git push origin master
 
 # Commit all changes of project files and push
 cd ..
-git add -A
+git add .
 git commit -m "Update blog files"
 git push origin master
 printf "\033[0;32mFinished!\033[0m\n"
