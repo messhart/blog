e7b11c9a0435a73c3e7d1c280ad201dd3f0659eb
messhart <dyfttym@protonmail.ch>
2021-02-13 13:53:49 +0800

    Update blog files

---
+++ content/about.md
@@ -0,0 +1,11 @@
+---
+title: "About"
+date: 2021-12-02
+tldr: Making GitHub Actions with Js Code
+tags: [About]
+---
+
+hello, this is me.
+
+
+
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# set -e # If a command fails then the deploy stops
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+hugo # Build a site to the ./public directory
+cd public
+printf "\033[0;32mBuilding...\033[0m\n"
+git add . # Add all changes to git
+printf "\033[0;32mBuilding...\033[0m\n"
+# Commit changes
+msg="Rebuild site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+
+# Push source and build repos
+git push origin master
+
+# Commit all changes of project files and push
+cd ..
+git add .
+git commit -m "Update blog files"
+git push origin master
+printf "\033[0;32mFinished!\033[0m\n"
